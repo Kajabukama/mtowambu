20a097a1f4193c8bfa5d46ae76e3a2f5caf9fea3
CTS <cts@CTSs-iMac-2.local>
2019-10-08 04:25:19 -0700

    congigured project

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App" style={ theme.bg }>
+      <header className="App-header">
+        <h1>Mto wa Mbu <br/>Cultural Tourism</h1>
+      </header>
+    </div>
+  );
+}
+
+const bg = require('./assets/imgs/bg.jpg');
+const theme = {
+  bg : {
+    backgroundImage: 'url('+ bg +')',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover'
+  }
+}
+export default App;
